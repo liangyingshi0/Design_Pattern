b6f119612bd645ffbc4a2ef3d9b2ef9c5d5a37a2
liangyingshi0 <1649020848@qq.com>
2020-05-04 23:35:00 +0800

    更新了点餐练习中的产品对象

---
+++ Design_Pattern/src/Work_Order/SetMeal.java
@@ -0,0 +1,27 @@
+package Work_Order;
+
+//充当建造者模式中的产品对象
+public class SetMeal {
+	private String drink;
+	private String food;
+	private String vegetable;
+	public String getDrink() {
+		return drink;
+	}
+	public void setDrink(String drink) {
+		this.drink = drink;
+	}
+	public String getFood() {
+		return food;
+	}
+	public void setFood(String food) {
+		this.food = food;
+	}
+	public String getVegetable() {
+		return vegetable;
+	}
+	public void setVegetable(String vegetable) {
+		this.vegetable = vegetable;
+	}
+	
+}
